1d0ad041adb5b55a1069fc6bce69eee208ceb3b2
Pau Mezzadra <paumezzadra@hotmail.com>
2021-03-25 14:01:56 -0300

    Add project files.

---
+++ LaConsolaEjercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaConsolaEjercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!");
+            Console.WriteLine("Presione cualquier tecla para continuar");
+            Console.ReadKey();
+ 
+        }
+    }
+}
